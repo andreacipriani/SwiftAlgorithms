d8d8bf85de8ec1c363ccad5f5ab43d0bea45a398
Andrea Cipriani <andrea.cipriani@soundcloud.com>
2017-10-28 17:31:20 +0200

    Initial Commit

---
+++ SwiftAlgorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftAlgorithms
+//
+//  Created by Andrea Cipriani on 28.10.17.
+//  Copyright © 2017 agc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
